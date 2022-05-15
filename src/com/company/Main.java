34e75ca8569c501b2d95aa359f5b0c70b2a8700a
Khadija Ghayur <Khadija.g2020@gmail.com>
2022-05-15 09:24:37 -0400

    this is code from 1 to 50

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        for(int i=1; i<=100; i++){
+        for(int i=1; i<=50; i++){
             System.out.println(i);
         }
     }
